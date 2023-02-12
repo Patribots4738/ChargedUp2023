2c97d1ffcd51618ddaef4aa23a77d930d7ea180d
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-02-12 02:21:55 -0800

    Refactor all files to "2 spaces" to match VSCode settings Remove AutoWaypoints.java

--- src/main/java/hardware/Swerve.java
+++ src/main/java/hardware/Swerve.java
@@ -15,199 +15,199 @@ import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import math.Constants.DriveConstants;
 
 public class Swerve {
-    private double speedMultiplier = 1;
+  private double speedMultiplier = 1;
 
-    private final MAXSwerveModule m_frontLeft = new MAXSwerveModule(
-            DriveConstants.FRONT_LEFT_DRIVING_CAN_ID,
-            DriveConstants.FRONT_LEFT_TURNING_CAN_ID,
-            DriveConstants.FRONT_LEFT_CHASSIS_ANGULAR_OFFSET);
+  private final MAXSwerveModule m_frontLeft = new MAXSwerveModule(
+      DriveConstants.FRONT_LEFT_DRIVING_CAN_ID,
+      DriveConstants.FRONT_LEFT_TURNING_CAN_ID,
+      DriveConstants.FRONT_LEFT_CHASSIS_ANGULAR_OFFSET);
 
-    private final MAXSwerveModule m_frontRight = new MAXSwerveModule(
-            DriveConstants.FRONT_RIGHT_DRIVING_CAN_ID,
-            DriveConstants.FRONT_RIGHT_TURNING_CAN_ID,
-            DriveConstants.FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET);
+  private final MAXSwerveModule m_frontRight = new MAXSwerveModule(
+      DriveConstants.FRONT_RIGHT_DRIVING_CAN_ID,
+      DriveConstants.FRONT_RIGHT_TURNING_CAN_ID,
+      DriveConstants.FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET);
 
-    private final MAXSwerveModule m_rearLeft = new MAXSwerveModule(
-            DriveConstants.REAR_LEFT_DRIVING_CAN_ID,
-            DriveConstants.REAR_LEFT_TURNING_CAN_ID,
-            DriveConstants.BACK_LEFT_CHASSIS_ANGULAR_OFFSET);
+  private final MAXSwerveModule m_rearLeft = new MAXSwerveModule(
+      DriveConstants.REAR_LEFT_DRIVING_CAN_ID,
+      DriveConstants.REAR_LEFT_TURNING_CAN_ID,
+      DriveConstants.BACK_LEFT_CHASSIS_ANGULAR_OFFSET);
 
-    private final MAXSwerveModule m_rearRight = new MAXSwerveModule(
-            DriveConstants.REAR_RIGHT_DRIVING_CAN_ID,
-            DriveConstants.REAR_RIGHT_TURNING_CAN_ID,
-            DriveConstants.BACK_RIGHT_CHASSIS_ANGULAR_OFFSET);
+  private final MAXSwerveModule m_rearRight = new MAXSwerveModule(
+      DriveConstants.REAR_RIGHT_DRIVING_CAN_ID,
+      DriveConstants.REAR_RIGHT_TURNING_CAN_ID,
+      DriveConstants.BACK_RIGHT_CHASSIS_ANGULAR_OFFSET);
 
-    // The gyro sensor
-    private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
+  // The gyro sensor
+  private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
 
-    // Odometry class for tracking robot pose
-    SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
-            DriveConstants.DRIVE_KINEMATICS,
-            Rotation2d.fromDegrees(m_gyro.getAngle()),
-            new SwerveModulePosition[]{
-                    m_frontLeft.getPosition(),
-                    m_frontRight.getPosition(),
-                    m_rearLeft.getPosition(),
-                    m_rearRight.getPosition()
-            });
+  // Odometry class for tracking robot pose
+  SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+      DriveConstants.DRIVE_KINEMATICS,
+      Rotation2d.fromDegrees(m_gyro.getAngle()),
+      new SwerveModulePosition[]{
+          m_frontLeft.getPosition(),
+          m_frontRight.getPosition(),
+          m_rearLeft.getPosition(),
+          m_rearRight.getPosition()
+      });
 
-    /**
-     * Creates a new DriveSubsystem.
-     */
-    public Swerve() {
-    }
+  /**
+   * Creates a new DriveSubsystem.
+   */
+  public Swerve() {
+  }
 
-    public void periodic() {
-        // Update the odometry in the periodic block
-        m_odometry.update(
-                Rotation2d.fromDegrees(m_gyro.getAngle()),
-                new SwerveModulePosition[]{
-                        m_frontLeft.getPosition(),
-                        m_frontRight.getPosition(),
-                        m_rearLeft.getPosition(),
-                        m_rearRight.getPosition()
-                });
-    }
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        Rotation2d.fromDegrees(m_gyro.getAngle()),
+        new SwerveModulePosition[]{
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        });
+  }
 
-    /**
-     * Returns the currently-estimated pose of the robot.
-     *
-     * @return The pose.
-     */
-    public Pose2d getPose() {
-        return m_odometry.getPoseMeters();
-    }
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
 
-    // Get the odometry object
-    public SwerveDriveOdometry getOdometry() {
-        return m_odometry;
-    }
+  // Get the odometry object
+  public SwerveDriveOdometry getOdometry() {
+    return m_odometry;
+  }
 
-    /**
-     * Resets the odometry to the specified pose.
-     *
-     * @param pose The pose to which to set the odometry.
-     */
-    public void resetOdometry(Pose2d pose) {
-        m_odometry.resetPosition(
-                Rotation2d.fromDegrees(m_gyro.getAngle()),
-                new SwerveModulePosition[]{
-                        m_frontLeft.getPosition(),
-                        m_frontRight.getPosition(),
-                        m_rearLeft.getPosition(),
-                        m_rearRight.getPosition()
-                },
-                pose);
-    }
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    m_odometry.resetPosition(
+        Rotation2d.fromDegrees(m_gyro.getAngle()),
+        new SwerveModulePosition[]{
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        },
+        pose);
+  }
 
-    /**
-     * Method to drive the robot using joystick info.
-     *
-     * @param xSpeed        Speed of the robot in the x direction (forward).
-     * @param ySpeed        Speed of the robot in the y direction (sideways).
-     * @param rotSpeed      Angular rate of the robot.
-     * @param fieldRelative Whether the provided x and y speeds are relative to the
-     *                      field.
-     */
-    public void drive(double xSpeed, double ySpeed, double rotSpeed, boolean fieldRelative) {
-        // Adjust input based on max speed
-        xSpeed *= DriveConstants.MAX_SPEED_METERS_PER_SECOND * speedMultiplier;
-        ySpeed *= DriveConstants.MAX_SPEED_METERS_PER_SECOND * speedMultiplier;
-        rotSpeed *= DriveConstants.MAX_ANGULAR_SPEED * speedMultiplier;
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rotSpeed      Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
+   */
+  public void drive(double xSpeed, double ySpeed, double rotSpeed, boolean fieldRelative) {
+    // Adjust input based on max speed
+    xSpeed *= DriveConstants.MAX_SPEED_METERS_PER_SECOND * speedMultiplier;
+    ySpeed *= DriveConstants.MAX_SPEED_METERS_PER_SECOND * speedMultiplier;
+    rotSpeed *= DriveConstants.MAX_ANGULAR_SPEED * speedMultiplier;
 
-        var swerveModuleStates = DriveConstants.DRIVE_KINEMATICS.toSwerveModuleStates(
-                fieldRelative
-                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rotSpeed, getOdometry().getPoseMeters().getRotation())
-                        : new ChassisSpeeds(xSpeed, ySpeed, rotSpeed));
+    var swerveModuleStates = DriveConstants.DRIVE_KINEMATICS.toSwerveModuleStates(
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rotSpeed, getOdometry().getPoseMeters().getRotation())
+            : new ChassisSpeeds(xSpeed, ySpeed, rotSpeed));
 
-        setModuleStates(swerveModuleStates);
-    }
+    setModuleStates(swerveModuleStates);
+  }
 
-    /**
-     * Sets the wheels into an X formation to prevent movement.
-     */
-    public void setX() {
-        m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
-        m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-        m_rearLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-        m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
-    }
+  /**
+   * Sets the wheels into an X formation to prevent movement.
+   */
+  public void setX() {
+    m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+    m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+  }
 
-    /**
-     * Sets the swerve ModuleStates.
-     *
-     * @param desiredStates The desired SwerveModule states.
-     */
-    public void setModuleStates(SwerveModuleState[] desiredStates) {
-        SwerveDriveKinematics.desaturateWheelSpeeds(
-                desiredStates, DriveConstants.MAX_SPEED_METERS_PER_SECOND);
-        m_frontLeft.setDesiredState(desiredStates[0]);
-        m_frontRight.setDesiredState(desiredStates[1]);
-        m_rearLeft.setDesiredState(desiredStates[2]);
-        m_rearRight.setDesiredState(desiredStates[3]);
-    }
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param desiredStates The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        desiredStates, DriveConstants.MAX_SPEED_METERS_PER_SECOND);
+    m_frontLeft.setDesiredState(desiredStates[0]);
+    m_frontRight.setDesiredState(desiredStates[1]);
+    m_rearLeft.setDesiredState(desiredStates[2]);
+    m_rearRight.setDesiredState(desiredStates[3]);
+  }
 
-    /**
-     * Resets the drive encoders to currently read a position of 0.
-     */
-    public void resetEncoders() {
-        m_frontLeft.resetEncoders();
-        m_rearLeft.resetEncoders();
-        m_frontRight.resetEncoders();
-        m_rearRight.resetEncoders();
-    }
+  /**
+   * Resets the drive encoders to currently read a position of 0.
+   */
+  public void resetEncoders() {
+    m_frontLeft.resetEncoders();
+    m_rearLeft.resetEncoders();
+    m_frontRight.resetEncoders();
+    m_rearRight.resetEncoders();
+  }
 
-    /**
-     * Zeroes the heading of the robot.
-     */
-    public void zeroHeading() {
-        m_gyro.reset();
-    }
+  /**
+   * Zeroes the heading of the robot.
+   */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
 
-    /**
-     * Returns the heading of the robot.
-     *
-     * @return the robot's heading in degrees, from -180 to 180
-     */
-    public double getHeading() {
-        return Rotation2d.fromDegrees(m_gyro.getAngle()).getDegrees();
-    }
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return Rotation2d.fromDegrees(m_gyro.getAngle()).getDegrees();
+  }
 
-    /**
-     * Returns the turn rate of the robot.
-     *
-     * @return The turn rate of the robot, in degrees per second
-     */
-    public double getTurnRate() {
-        return m_gyro.getRate() * (DriveConstants.GYRO_REVERSED ? -1.0 : 1.0);
-    }
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return m_gyro.getRate() * (DriveConstants.GYRO_REVERSED ? -1.0 : 1.0);
+  }
 
-    public void toggleSpeed() {
-      this.speedMultiplier = (this.speedMultiplier == 1) ? 0.1 : 1;
-    }
+  public void toggleSpeed() {
+    this.speedMultiplier = (this.speedMultiplier == 1) ? 0.1 : 1;
+  }
 
-    /**
-     * Sets the coast mode for the drive motors.
-     * This is useful for when the robot is disabled
-     * So we can freely move the robot around
-     */
-    public void setCoastMode() {
-        m_frontLeft.setCoastMode();
-        m_frontRight.setCoastMode();
-        m_rearLeft.setCoastMode();
-        m_rearRight.setCoastMode();
-    }
+  /**
+   * Sets the coast mode for the drive motors.
+   * This is useful for when the robot is disabled
+   * So we can freely move the robot around
+   */
+  public void setCoastMode() {
+    m_frontLeft.setCoastMode();
+    m_frontRight.setCoastMode();
+    m_rearLeft.setCoastMode();
+    m_rearRight.setCoastMode();
+  }
 
-    /**
-     * Sets the brake mode for the drive motors.
-     * This is useful for when the robot is enabled
-     * So we can stop the robot quickly
-     * (This is the default mode)
-     */
-    public void setBrakeMode() {
-        m_frontLeft.setBrakeMode();
-        m_frontRight.setBrakeMode();
-        m_rearLeft.setBrakeMode();
-        m_rearRight.setBrakeMode();
-    }
+  /**
+   * Sets the brake mode for the drive motors.
+   * This is useful for when the robot is enabled
+   * So we can stop the robot quickly
+   * (This is the default mode)
+   */
+  public void setBrakeMode() {
+    m_frontLeft.setBrakeMode();
+    m_frontRight.setBrakeMode();
+    m_rearLeft.setBrakeMode();
+    m_rearRight.setBrakeMode();
+  }
 }
--- src/main/java/math/OICalc.java
+++ src/main/java/math/OICalc.java
@@ -7,67 +7,67 @@ import math.Constants.OIConstants;
 public class OICalc {
 
 
-    // All calculations can be refrenced here https://www.desmos.com/calculator/e07raajzh5
-    public static Translation2d toCircle(double x, double y) {
+  // All calculations can be refrenced here https://www.desmos.com/calculator/e07raajzh5
+  public static Translation2d toCircle(double x, double y) {
 
-        Translation2d intercept;
-        Translation2d output;
+    Translation2d intercept;
+    Translation2d output;
 
-        double slope = y / x;
-        if (slope == 0 || Double.isNaN(slope) || Double.isInfinite(slope)) {
+    double slope = y / x;
+    if (slope == 0 || Double.isNaN(slope) || Double.isInfinite(slope)) {
 
-            output = new Translation2d(x, y);
-            return output;
+      output = new Translation2d(x, y);
+      return output;
 
-        } else if (0 > slope && slope >= -OIConstants.CONTROLLER_CORNER_SLOPE_2) {
+    } else if (0 > slope && slope >= -OIConstants.CONTROLLER_CORNER_SLOPE_2) {
 
-            intercept = new Translation2d(-1, -slope);
+      intercept = new Translation2d(-1, -slope);
 
-        } else if (-OIConstants.CONTROLLER_CORNER_SLOPE_1 < slope && slope < -OIConstants.CONTROLLER_CORNER_SLOPE_2) {
+    } else if (-OIConstants.CONTROLLER_CORNER_SLOPE_1 < slope && slope < -OIConstants.CONTROLLER_CORNER_SLOPE_2) {
 
-            double intersectionX = (1.7) / (slope - 1);
-            double intersectionY = (intersectionX + 1.7);
+      double intersectionX = (1.7) / (slope - 1);
+      double intersectionY = (intersectionX + 1.7);
 
-            intercept = new Translation2d(intersectionX, intersectionY);
+      intercept = new Translation2d(intersectionX, intersectionY);
 
-        } else if (slope < -OIConstants.CONTROLLER_CORNER_SLOPE_1 || slope > OIConstants.CONTROLLER_CORNER_SLOPE_1) {
+    } else if (slope < -OIConstants.CONTROLLER_CORNER_SLOPE_1 || slope > OIConstants.CONTROLLER_CORNER_SLOPE_1) {
 
-            intercept = new Translation2d(1 / slope, 1);
+      intercept = new Translation2d(1 / slope, 1);
 
-        } else if (OIConstants.CONTROLLER_CORNER_SLOPE_1 > slope && slope > OIConstants.CONTROLLER_CORNER_SLOPE_2) {
+    } else if (OIConstants.CONTROLLER_CORNER_SLOPE_1 > slope && slope > OIConstants.CONTROLLER_CORNER_SLOPE_2) {
 
-            double intersectionX = (1.7) / (slope + 1);
-            double intersectionY = -intersectionX + 1.7;
+      double intersectionX = (1.7) / (slope + 1);
+      double intersectionY = -intersectionX + 1.7;
 
-            intercept = new Translation2d(intersectionX, intersectionY);
+      intercept = new Translation2d(intersectionX, intersectionY);
 
-        } else if (0 < slope && slope <= OIConstants.CONTROLLER_CORNER_SLOPE_2) {
+    } else if (0 < slope && slope <= OIConstants.CONTROLLER_CORNER_SLOPE_2) {
 
-            intercept = new Translation2d(1, slope);
+      intercept = new Translation2d(1, slope);
 
-        } else {
+    } else {
 
-            intercept = new Translation2d(0, 0);
+      intercept = new Translation2d(0, 0);
 
-            System.out.println("Error... Slope = " + slope);
+      System.out.println("Error... Slope = " + slope);
 
-        }
-
-        double distance = getDistance(x, y, intercept.getX(), intercept.getY());
-
-        distance = (distance > 1) ? 1 : distance;
-
-        output = new Translation2d(Math.signum(x) * distance, new Rotation2d(Math.atan(y / x)));
-
-        return output;
     }
 
-    public static double getDistance(double x1, double y1, double x2, double y2) {
-        return
-                Math.sqrt(
-                        (Math.pow(x1, 2) + Math.pow(y1, 2))
-                                /
-                                (Math.pow(x2, 2) + Math.pow(y2, 2)));
-    }
+    double distance = getDistance(x, y, intercept.getX(), intercept.getY());
+
+    distance = (distance > 1) ? 1 : distance;
+
+    output = new Translation2d(Math.signum(x) * distance, new Rotation2d(Math.atan(y / x)));
+
+    return output;
+  }
+
+  public static double getDistance(double x1, double y1, double x2, double y2) {
+    return
+        Math.sqrt(
+            (Math.pow(x1, 2) + Math.pow(y1, 2))
+                /
+                (Math.pow(x2, 2) + Math.pow(y2, 2)));
+  }
 
 }
