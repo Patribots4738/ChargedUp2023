164a32c84d0d9999e1f565fcb72c4c600f80a805
ChadPad <97857553+PatribotsProgramming@users.noreply.github.com>
2023-03-10 13:11:05 -0800

    speed up swerve's slew rate limiting

--- src/main/java/calc/Constants.java
+++ src/main/java/calc/Constants.java
@@ -28,11 +28,11 @@ public final class Constants {
         public static double MAX_SPEED_METERS_PER_SECOND = AutoConstants.MAX_SPEED_METERS_PER_SECOND;
         public static double MAX_ANGULAR_SPEED = 4 * Math.PI; // radians per second
 
-        public static final double MAX_TELEOP_SPEED_METERS_PER_SECOND = 3;
+        public static final double MAX_TELEOP_SPEED_METERS_PER_SECOND = 4;
 
-        public static final double kDirectionSlewRate = 1.2; // radians per second
-        public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-        public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+        public static final double kDirectionSlewRate = 3.14; // radians per second
+        public static final double kMagnitudeSlewRate = 65.0; // percent per second (1 = 100%)
+        public static final double kRotationalSlewRate = 65.0; // percent per second (1 = 100%)
 
         // Chassis configuration
         // Distance between centers of right and left wheels on robot
