a213ea4f23575706986156b478d3213410336d6a
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-03-18 23:47:09 -0700

    Only use the thetaController in the HDC to calculate our rotational speed when snapping

--- src/main/java/auto/AutoAlignment.java
+++ src/main/java/auto/AutoAlignment.java
@@ -466,13 +466,10 @@ public class AutoAlignment implements Loggable{
     public void snapToAngle(Translation2d driverAxis, Rotation2d desiredAngle) {
 
       // Use a Holonomic Drive Controller to calculate the speeds for the robot
-      // Notice that the desiredLinearVelocity is set to zero, because we don't care about driving
-      // We only care about turning, and in the caluclate method the desiredLinearVelocity is not used for our use case
-      ChassisSpeeds trajectorySpeeds = SwerveTrajectory.HDC.calculate(swerve.getPose(), swerve.getPose(), 0, desiredAngle);
-      
+      double thetaSpeed = SwerveTrajectory.HDC.getThetaController().calculate(desiredAngle.getRadians());
       // Notice that only the turning speed is used. We still want to be able to drive forward and strafe
       // One strange thing that I noticed is that the HDC generally doesn't use field relative to drive,
       // I wonder if that will cause issues in the future.
-      swerve.drive(driverAxis.getY(), driverAxis.getX(), trajectorySpeeds.omegaRadiansPerSecond, true, true);
+      swerve.drive(driverAxis.getY(), driverAxis.getX(), thetaSpeed, true, true);
     }
 }
