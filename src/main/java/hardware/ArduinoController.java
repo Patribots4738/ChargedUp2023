64a6ced76cea457ae7e76c1aa8d7ec79422d34a4
Hamilton <a.hamilton72006@gmail.com>
2023-03-20 12:18:41 -0700

    Only send bytes to the arduino if the current state is not the desired state

--- src/main/java/hardware/ArduinoController.java
+++ src/main/java/hardware/ArduinoController.java
@@ -6,9 +6,12 @@ public class ArduinoController {
   
   public static final int ARDUINO_ADDRESS = 8;
   public final I2C arduino = new I2C(I2C.Port.kOnboard, ARDUINO_ADDRESS); //Sets up the Arduino over I2C on port 8
-
+  private int currentValue = 0;
   public void sendByte(int value) {
-    arduino.write(ARDUINO_ADDRESS, value);
+    if (value != currentValue) {
+      currentValue = value;
+      arduino.write(ARDUINO_ADDRESS, value);
+    }
   }
 
 }
