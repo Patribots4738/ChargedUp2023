7ccbff3a4bb98273b27bad72a130bd31ef07e6f5
ChadPad <97857553+PatribotsProgramming@users.noreply.github.com>
2023-03-20 21:06:16 -0700

    one small step for light, preparing for one big leap for light kind

--- src/main/java/hardware/ArduinoController.java
+++ src/main/java/hardware/ArduinoController.java
@@ -1,16 +1,37 @@
 package hardware;
 
+import calc.Constants.LEDConstants;
 import edu.wpi.first.wpilibj.I2C;
 
 public class ArduinoController {
   
   public static final int ARDUINO_ADDRESS = 8;
-  public final I2C arduino = new I2C(I2C.Port.kOnboard, ARDUINO_ADDRESS); //Sets up the Arduino over I2C on port 8
-  private int currentValue = 0;
+  private int currentValue = -1;
+  private boolean newLoop = true;
+
+  //Sets up the Arduino over I2C on port 8
+  public final I2C arduino = new I2C(I2C.Port.kOnboard, ARDUINO_ADDRESS);
+  
   public void sendByte(int value) {
-    if (value != currentValue) {
+    
+    if (value != currentValue && newLoop) {
+      newLoop = false;
+    
       currentValue = value;
-      arduino.write(ARDUINO_ADDRESS, value);
+          
+      if (value == LEDConstants.BELLY_PAN_PURPLE || 
+          value == LEDConstants.BELLY_PAN_YELLOW || 
+          value == LEDConstants.BELLY_PAN_RAINBOW) 
+      {
+        arduino.writeBulk(new byte[] {((byte) (value)), ((byte) (value + 20))});
+      }
+      else {
+        arduino.write(ARDUINO_ADDRESS, value);
+      }
     }
+  }
+
+  public void reset() {
+    this.newLoop = true;
   }
 }
