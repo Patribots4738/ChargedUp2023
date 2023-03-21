1ce0cd28ed50bae43100430063b180f026a316a4
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2023-03-21 11:14:28 -0700

    Swap the controller and change the code to accomodate it, slow down flashing

--- src/main/java/hardware/ArduinoController.java
+++ src/main/java/hardware/ArduinoController.java
@@ -8,9 +8,6 @@ public class ArduinoController {
   public final I2C arduino = new I2C(I2C.Port.kOnboard, ARDUINO_ADDRESS); //Sets up the Arduino over I2C on port 8
   private int currentValue = 0;
   public void sendByte(int value) {
-    if (value != currentValue) {
-      currentValue = value;
       arduino.write(ARDUINO_ADDRESS, value);
-    }
   }
 }
