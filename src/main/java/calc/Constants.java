080945787ad81c97418ca9327d40a69f1455ce19
ChadPad <patribotsprogramming4738@gmail.com>
2023-04-05 20:30:44 -0700

    Speed up auto constants t r e a d :>

--- src/main/java/calc/Constants.java
+++ src/main/java/calc/Constants.java
@@ -139,8 +139,8 @@ public final class Constants {
 
     public static final class AutoConstants {
 
-        public static final double MAX_SPEED_METERS_PER_SECOND = 2.65; // 2.5 has worked very well for us so far
-        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 3; // 2.5; (2.5vel and 2.5accel output 3s runtime on _1_A)
+        public static final double MAX_SPEED_METERS_PER_SECOND = 3; // 2.5 has worked very well for us so far
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 3.25; // 2.5; (2.5vel and 2.5accel output 3s runtime on _1_A)
         public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = 2 * Math.PI;
         public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED = Math.PI*4;//Math.PI/1.7;
 
