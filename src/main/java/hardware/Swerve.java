9ffd302b7fc8c97d515b3cd2d083d9c733c5c530
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-09-11 01:46:05 -0700

    roll,pitch,yaw

--- src/main/java/hardware/Swerve.java
+++ src/main/java/hardware/Swerve.java
@@ -352,11 +352,8 @@ public class Swerve {
 
   public double getTilt() {
 
-    Rotation3d gyroRotation3d = new Rotation3d(getRoll().getRadians(), getYaw().getRadians(),
-        getPitch().getRadians());
-
-    // System.out.println(getRoll().getDegrees() + " " + getYaw().getDegrees() + " "
-    // + getPitch().getDegrees());
+    Rotation3d gyroRotation3d = new Rotation3d(getRoll().getRadians(), getPitch().getRadians(),
+        getYaw().getRadians());
 
     Quaternion gyroQuaternion = gyroRotation3d.getQuaternion();
     // Multiply the quaternion by the UP direction as a vector to normalize it to UP
