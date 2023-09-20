ff1d79f071e2200c5e5be4e68769894f21abaad6
Alexander Hamilton <65139378+GalexY727@users.noreply.github.com>
2023-09-20 10:00:21 -0700

    Set halfwayFinishedWithConeFlipIndex to become false after finishing
    This will make it so that the claw is not constantly being told to go at max speed when hitting "B" on operator.

--- src/main/java/hardware/Arm.java
+++ src/main/java/hardware/Arm.java
@@ -331,7 +331,7 @@ public class Arm implements Loggable{
 
     public boolean halfwayFinishedWithConeFlip() {
         return this.armPosDimension1 == PlacementConstants.CONE_FLIP_INDEX && 
-                this.armPosDimension2 >= PlacementConstants.ARM_POSITIONS[armPosDimension1].length-1;
+                this.armPosDimension2 >= PlacementConstants.ARM_POSITIONS[armPosDimension1].length-1 && !armsAtDesiredPositions;
     }
 
     /**
