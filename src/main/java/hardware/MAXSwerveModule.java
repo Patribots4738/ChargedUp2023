af071a18624ee57374a1306f8174af3db7b64689
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2023-10-20 11:43:05 -0700

    Silly goofy unused import

--- src/main/java/hardware/MAXSwerveModule.java
+++ src/main/java/hardware/MAXSwerveModule.java
@@ -14,7 +14,6 @@ import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.Timer;
 import calc.Constants.ModuleConstants;
 import calc.Constants.NeoMotorConstants;
 
