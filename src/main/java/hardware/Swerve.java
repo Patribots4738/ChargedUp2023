4a2149a6620ea5467f5250ffeef48a18d4479b35
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-10-29 22:36:34 -0700

    Is that an unused import 🤨

--- src/main/java/hardware/Swerve.java
+++ src/main/java/hardware/Swerve.java
@@ -17,7 +17,6 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.WPIUtilJNI;
 import calc.ADIS16470_IMU;
 import calc.Pose3dLogger;
